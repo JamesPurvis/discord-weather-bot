e615123894bf781c26c4c3d4823f6f7ea7648645
James Purvis <110248591+JamesPurvis@users.noreply.github.com>
2023-06-07 17:39:36 -0600

    intial commit

---
+++ main.py
@@ -0,0 +1,46 @@
+import json
+
+import discord
+from discord.ext import commands
+
+import requests
+
+from config import API_KEY, BOT_TOKEN
+
+intents = discord.Intents.default()
+intents.typing = False
+intents.presences = False
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'Logged in as {bot.user.name} ({bot.user.id})')
+
+
+@bot.event
+async def on_message(message):
+        if message.content.startswith("!weather"):
+            param = message.content.split(' ')
+            json_data = process_request(param[1])
+            location_name = json_data["location"]["name"]
+            current_temp = json_data["current"]["temp_f"]
+            current_condition_text = json_data["current"]["condition"]["text"]
+
+            await message.channel.send("It is currently" + " " + str(current_temp) + " " + "and " + current_condition_text + " " + "in " + location_name)
+
+def process_request(param):
+    response = requests.get('http://api.weatherapi.com/v1/current.json?key=' + API_KEY + '&q=' + str(param) + '&aqi=no')
+
+    if response.status_code == 200:
+        return response.json()
+    else:
+        print("Response failed with status code: ", response.status_code)
+
+
+
+
+
+
+bot.run(BOT_TOKEN);
+
